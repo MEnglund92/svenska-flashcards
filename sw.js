5494630b11210409307d4bc94ce96b05f3c556bb
MEnglund92 <mattias.englund1992@gmail.com>
2026-08-09 13:04:32 +0200

    fix(sw): bump cache to v16 to flush stale index.html

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'svenska-v15';
+const CACHE = 'svenska-v16';
 const FILES = ['.','index.html','data.js','data_deck.js','manifest.json','novels_reading.json','icons/icon-192.png','icons/icon-512.png'];
 
 self.addEventListener('install', e => {
